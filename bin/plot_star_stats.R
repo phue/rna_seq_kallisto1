cd1d8dc8487026917a488bfd27a74956f88166e8
Elin Axelsson <elinaxel@gmail.com>
2018-02-16 10:45:02 +0000

    Plotting star stats with color in ggplot

--- bin/plot_star_stats.R
+++ bin/plot_star_stats.R
@@ -1,26 +1,27 @@
 #!/usr/bin/env Rscript
 
+library(dplyr)
+library(ggplot2)
+library(reshape2)
 
 args <- commandArgs(TRUE)
 star_stat=read.table("star_stats.tab",sep="\t",header=T,stringsAsFactors = F)
-star_stat = dplyr::mutate(star_stat,ID=sapply(strsplit(ID,"Log"),"[[",1) )
-
+star_stat = mutate(star_stat,sample=sapply(strsplit(ID,"Log"),"[[",1) )
+star_stat = star_stat %>% select (-c(ID,X))
 
 
 s2c <- read.table(args[1], header = TRUE, stringsAsFactors=FALSE)
-s2c <- dplyr::mutate(s2c, name = paste(condition,sample,sep="_"))
-s2c <- dplyr::select(s2c, sample = run_accession, condition, name)
+s2c <- mutate(s2c, name = paste(condition,sample,sep="_"))
+s2c <-select(s2c, sample = run_accession, condition, name)
 
-#star_stat dplyr::mutate(star_stat,name = sc2$name[s2c$sample==])
+star_stat = inner_join(star_stat,s2c)
 
+star_stat = star_stat %>% select (-c(sample,condition))
 
-
-#star_mat = star_stat[,-1]
-#rownames(star_mat) = sapply(strsplit(star_stat[,1],"_"),"[[",1) 
-#star_mat = star_mat[,!is.na(colSums(star_mat))]
+stat_mat = melt(star_stat, id = c("name"))
 
 pdf("star_stats.pdf")
-for (i in 1:ncol(star_mat)){
-  barplot(star_mat[,i],names.arg=rownames(star_mat),las=2,main=colnames(star_mat)[i])
-}
+ggplot(stat_mat,aes(x=name,y=value,fill=name))+ geom_bar(stat="identity")+facet_wrap(~variable,scales = "free")+ guides(fill=FALSE)+
+  theme(axis.title.x=element_blank(),axis.title.y=element_blank())
 dev.off()
+
